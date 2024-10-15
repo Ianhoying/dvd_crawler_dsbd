0e87854650e78e88f21382c194cc5b88f6c0cf8a
Ianhoying <145854610+Ianhoying@users.noreply.github.com>
2024-10-15 15:00:06 +0900

    Update dsbd.py

--- dsbd.py
+++ dsbd.py
@@ -1,7 +1,7 @@
 import streamlit as st, pandas as pd, numpy as np, matplotlib as plt
 
 
-st.title("미국 배당성장주 배당내역 크롤링 모니터링 대시보드")
+st.title("배당성장주 배당내역 모니터링 대시보드")
 
 n = pd.read_csv('n.csv', encoding = 'EUC-KR').groupby('티커').head(5).copy()
 s = pd.read_csv('s.csv', encoding = 'EUC-KR').groupby('티커').head(5).copy()
