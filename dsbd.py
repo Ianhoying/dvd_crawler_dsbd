1889097c7fa8d7992a4a3f4fd337fa9c1904a830
Ianhoying <145854610+Ianhoying@users.noreply.github.com>
2024-10-15 09:04:56 +0900

    Update dsbd.py

--- dsbd.py
+++ dsbd.py
@@ -1,4 +1,8 @@
 import streamlit as st, pandas as pd, numpy as np, matplotlib as plt
 
 
-st.title("ㅎㅇㅎㅇ 테스트다")
+st.title("미국 배당성장주 배당내역 크롤링 모니터링 대시보드")
+
+details = pd.read_csv('update_details.csv', encoding = 'EUC-KR')
+
+st.dataframe(details)
