029e77e3d03c4fd0ae366f7fb5d84fbfb46243bc
Ianhoying <145854610+Ianhoying@users.noreply.github.com>
2024-10-15 14:48:09 +0900

    Update dsbd.py

--- dsbd.py
+++ dsbd.py
@@ -8,14 +8,19 @@ s = pd.read_csv('s.csv', encoding = 'EUC-KR').groupby('티커').head(5).copy()
 y = pd.read_csv('y.csv', encoding = 'EUC-KR').groupby('티커').head(5).copy()
 d = pd.read_csv('d.csv', encoding = 'EUC-KR').groupby('티커').head(5).copy()
 
+y_splits = pd.read_csv('y_splits.csv', encoding = 'EUC-KR')
+d_splits = pd.read_csv('d_splits.csv', encoding = 'EUC-KR')
 # details = pd.read_csv('update_details.csv', encoding = 'EUC-KR')
 
 
 
 
-st.dataframe(n)
-st.dataframe(s)
-st.dataframe(y)
-st.dataframe(d)
+# st.dataframe(n)
+# st.dataframe(s)
+# st.dataframe(y)
+# st.dataframe(d)
+
+st.dataframe(y_splits)
+st.dataframe(d_splits)
 
 
