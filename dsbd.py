060033af5e40b464fe79d70416bbebd2d16af46d
Ianhoying <145854610+Ianhoying@users.noreply.github.com>
2024-10-15 09:09:24 +0900

    Update dsbd.py

--- dsbd.py
+++ dsbd.py
@@ -3,11 +3,16 @@ import streamlit as st, pandas as pd, numpy as np, matplotlib as plt
 
 st.title("미국 배당성장주 배당내역 크롤링 모니터링 대시보드")
 
-n = pd.read_csv('n.csv', encoding = 'EUC-KR')
-s = pd.read_csv('s.csv', encoding = 'EUC-KR')
-y = pd.read_csv('y.csv', encoding = 'EUC-KR')
-d = pd.read_csv('d.csv', encoding = 'EUC-KR')
+n = pd.read_csv('n.csv', encoding = 'EUC-KR').groupby('티커').head(15).copy()
+s = pd.read_csv('s.csv', encoding = 'EUC-KR').groupby('티커').head(15).copy()
+y = pd.read_csv('y.csv', encoding = 'EUC-KR').groupby('티커').head(15).copy()
+d = pd.read_csv('d.csv', encoding = 'EUC-KR').groupby('티커').head(15).copy()
 
-details = pd.read_csv('update_details.csv', encoding = 'EUC-KR')
+# details = pd.read_csv('update_details.csv', encoding = 'EUC-KR')
 
 st.dataframe(n)
+st.dataframe(s)
+st.dataframe(y)
+st.dataframe(d)
+
+
