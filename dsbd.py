d5cf4b253ba0d1eca4bb17f64fe68baad72f7fab
Ianhoying <145854610+Ianhoying@users.noreply.github.com>
2024-10-17 15:45:20 +0900

    Update dsbd.py

--- dsbd.py
+++ dsbd.py
@@ -23,66 +23,83 @@ def df_divide(df, gubun):
             continue
         else:
             df = df.rename(columns = {i : i + '_' + gubun})
-
-    f = df[pd.to_datetime(df['배당락일']) > pd.to_datetime(datetime.datetime.today())].reset_index(drop = True).copy()
-    p = df[pd.to_datetime(df['배당락일']) <= pd.to_datetime(datetime.datetime.today())].reset_index(drop = True).copy()
     
-    return f, p
+    return df
 
-n_future, n_past = df_divide(n, 'n')
-s_future, s_past = df_divide(s, 's')
-y_future, y_past = df_divide(y, 'y')
-d_future, d_past = df_divide(d, 'd')
+def df_total(n, s, y, d):
 
-def div_unequal(n, s, y, d):
+    _n = df_divide(n, 'n')
+    _s = df_divide(s, 's')
+    _y = df_divide(y, 'y')
+    _d = df_divide(d, 'd')
 
-    total_future = \
-        pd.merge(d, \
-                 pd.merge(y, \
-                          pd.merge(n, s, 'outer', ['티커', '배당락일']),\
+    n_li = ['티커', '배당락일', '배당금액_n' , 'n']
+    s_li = ['티커', '배당락일', '배당금액_s','수정배당금액_s', '조정팩터_s', '지급주기_s', '배당유형_s', 's']
+    y_li = ['티커', '배당락일', '수정배당금액_y', 'y']
+    d_li = ['티커', '배당락일', '수정배당금액_d', 'd']
+
+    total = \
+        pd.merge(_d[d_li], \
+                 pd.merge(_y[y_li], \
+                          pd.merge(_n[n_li], _s[s_li], 'outer', ['티커', '배당락일']),\
                           'outer', ['티커', '배당락일']),\
                  'outer', ['티커', '배당락일'])
 
-    # 정기배당만 남김
-    total_future = total_future[total_future['배당유형_s'] == 'Regular'].reset_index(drop = True).copy()
+    # Regualr dividends
+    total = total[total['배당유형_s'] == 'Regular'].reset_index(drop = True).copy()
 
-    # nasdaq, seekingalpha, digrin 모두 갖고 있는 배당내역인 경우만 추출
-    total_future_Y =\
-        total_future[(total_future['n'] == 'Y') &
-                    (total_future['s'] == 'Y') &
-                    (total_future['d'] == 'Y')][total_future.columns.sort_values()].reset_index(drop = True)
+    # Only Y Y Y
+    total = \
+    total[(total['n'] == 'Y') &
+    (total['s'] == 'Y') &
+    (total['d'] == 'Y')][['티커', '배당락일',\
+                          '배당금액_n', '배당금액_s', '수정배당금액_s', '수정배당금액_y', '수정배당금액_d',\
+                          '조정팩터_s', '지급주기_s', '배당유형_s']].sort_values(['티커', '배당락일'], ascending = [True, False]).reset_index(drop = True)
 
-def div_unequal(n, s, y, d):
+    # Recent info shift
+    for i in total.columns:
+        if i == '티커':
+            continue
+        else:
+            total['직전_' + i] = total.groupby('티커').shift(-1)[i]
 
-	total_future = \
-	pd.merge(d, \
-		 pd.merge(y, \
-			  pd.merge(n, s, 'outer', ['티커', '배당락일']),\
-			  'outer', ['티커', '배당락일']),\
-		 'outer', ['티커', '배당락일'])
+    total = total[['티커', '배당락일', '직전_배당락일',\
+                   '배당금액_n', '직전_배당금액_n', '배당금액_s', '직전_배당금액_s',\
+                   '수정배당금액_s', '직전_수정배당금액_s', '수정배당금액_y', '직전_수정배당금액_y', '수정배당금액_d', '직전_수정배당금액_d',\
+                   '조정팩터_s', '직전_조정팩터_s', '지급주기_s', '직전_지급주기_s', '배당유형_s',  '직전_배당유형_s']]
+    
+    f = total[pd.to_datetime(total['배당락일']) > pd.to_datetime(datetime.datetime.today())].reset_index(drop = True).copy()
+    p = total[pd.to_datetime(total['배당락일']) <= pd.to_datetime(datetime.datetime.today())].reset_index(drop = True).copy()
 
-	# 정기배당만 남김
-	total_future = total_future[total_future['배당유형_s'] == 'Regular'].reset_index(drop = True).copy()
+    recent = pd.concat([p.groupby('티커').head(1), f.groupby('티커').tail(1)], axis = 0)\
+    .sort_values(['티커', '배당락일'], ascending = [True, False]).groupby('티커').tail(1).reset_index(drop = True)
+                          
+    return total, f, p, recent
+	
 
-	# nasdaq, seekingalpha, digrin 모두 갖고 있는 배당내역인 경우만 추출
-	total_future_Y =\
-	total_future[(total_future['n'] == 'Y') &
-		    (total_future['s'] == 'Y') &
-		    (total_future['d'] == 'Y')][total_future.columns.sort_values()].reset_index(drop = True)
+total, total_future, total_past, recent = df_total(n, s, y, d)
 
-	total_future_Y[ ~((total_future_Y['배당금액_n'] == total_future_Y['수정배당금액_s']) &\
-		  (total_future_Y['배당금액_n'] == total_future_Y['수정배당금액_d']) &\
-		  (total_future_Y['수정배당금액_s'] == total_future_Y['수정배당금액_d']))]
+# 직전대비 줄어든 경우
+div_cut = \
+recent[
+(recent['배당금액_n'] < recent['직전_배당금액_n'])|
+(recent['수정배당금액_s'] < recent['직전_수정배당금액_s'])|
+(recent['수정배당금액_d'] < recent['직전_수정배당금액_d'])
+].reset_index(drop = True)
 
-
+# 사이트별로 서로 다른 경우
+div_check = \
+recent[
+(recent['배당금액_n'] != recent['수정배당금액_s'])|
+(recent['수정배당금액_s'] != recent['수정배당금액_d'])|
+(recent['수정배당금액_d'] != recent['배당금액_n'])
+].reset_index(drop = True)
 
 total_splits = pd.merge(y_splits, d_splits, 'outer', ['티커', '권리락일'])
 total_splits['분할/병합'] = total_splits['분할/병합_y']
-# total_splits[total_splits['분할/병합_x'].isnull(), '분할/병합'] = total_splits['분할/병합_y']
 total_splits.loc[total_splits['분할/병합_y'].isnull(), '분할/병합'] = total_splits['분할/병합_x']
-
-
-
+total_splits = total_splits.drop(['분할/병합_x', '분할/병합_y'], axis = 1)
+total_splits['분할/병합 여부'] = 'Y'
 
 # Dashboard 
 
@@ -107,13 +124,16 @@ with tab1:
 	
 	st.subheader('배당감소 의심 종목\n\n')
 	st.caption('- 배당내역 크롤링 사이트 3곳 모두 해당 내역 존재\n- 주식분할/병합 여부\n- 배당지급 주기 변동 여부')
-	st.dataframe(n, hide_index = True, width = 2000, height = 1000)
+	st.dataframe(div_cut, hide_index = True, width = 2000, height = 1000)
 
 	st.subheader('배당내역 불일치 종목\n\n')
 	st.caption('')
+	st.dataframe(div_check, hide_index = True, width = 2000, height = 1000)
 	
 	st.subheader('주식 분할/병합 예정 종목\n\n')
 	st.caption('')
+	st.dataframe(total_splits, hide_index = True, width = 2000, height = 1000)
+
 	
 ### 02.배당내역
 with tab2:
