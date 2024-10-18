b9642dd5ee50c1e175926f190fb31047ea0f8331
Ianhoying <145854610+Ianhoying@users.noreply.github.com>
2024-10-18 12:50:04 +0900

    Update dsbd.py

--- dsbd.py
+++ dsbd.py
@@ -68,10 +68,11 @@ with tab1:
 	t1_r2c1, t1_r2c2 = st.columns(2)
 
 	with t1_r2c1:
-		st.markdown('배당감소 종목 수 : :blue-background'+str(len(div_cut))+' 개')
-		st.markdown('''
-		    :red[Streamlit] :orange[can] :green[write] :blue[text] :violet[in]
-		    :gray[pretty] :rainbow[colors] and :blue-background[highlight] text.''')
+		cnt = str(len(div_cut))
+		st.markdown('배당감소 종목 수 : :blue-background'{cnt}' 개')
+		# st.markdown('''
+		#     :red[Streamlit] :orange[can] :green[write] :blue[text] :violet[in]
+		#     :gray[pretty] :rainbow[colors] and :blue-background[highlight] text.''')
 		st.dataframe(div_cut, hide_index = True, width = 2000, height = 300, column_order = ('티커', '지급주기 변동', '분할/병합 여부', '분할/병합', '권리락일',\
 												     '직전_배당락일', '배당락일',\
 												     '직전_배당금액_n', '배당금액_n', '직전_배당금액_s', '배당금액_s',\
